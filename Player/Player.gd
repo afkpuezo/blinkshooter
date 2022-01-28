16c27a77fd2473ba1efd4f5e266b55f35c82034a
afkpuezo <andrewfcurry@gmail.com>
2022-01-28 15:15:16 -0500

    fixed friction

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,7 @@ class_name Player
 # movement speed values
 export var ACCELERATION := 500
 export var MAX_SPEED := 100
-export var FRICTION := 600
+export(float, 0.0, 1.0) var FRICTION := 0.1 # only applied when no movement input
 
 
 var velocity := Vector2.ZERO
@@ -21,8 +21,8 @@ func _physics_process(delta: float) -> void:
 	_input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	_input_vector = _input_vector.normalized()
 
-	if _input_vector != Vector2.ZERO:
+	if _input_vector:
 		velocity = velocity.move_toward(_input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * velocity.length())
 	velocity = move_and_slide(velocity)
