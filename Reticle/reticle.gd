ca615e8de5bf08911745e2997f7e4ea38483eb8b
afkpuezo <andrewfcurry@gmail.com>
2022-01-29 22:06:24 -0500

    basic mouse and controller reticle

--- Reticle/reticle.gd
+++ Reticle/reticle.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 class_name Reticle
 ## reticle used for player aiming
 ## should respond to mouse and/or controller input
@@ -6,28 +6,18 @@ class_name Reticle
 
 
 enum ReticleState {MOUSE, CONTROLLER, DISABLED}
+
 var current_state = ReticleState.DISABLED
-onready var cursor_image = $Sprite.texture
+onready var sprite := $Sprite
+onready var cursor_image = sprite.texture
+
+export var MAX_SPEED := 800
+var velocity := Vector2.ZERO
+var is_mouse_moving := false
 
 
 func _ready() -> void:
 	change_state(ReticleState.MOUSE)
-
-
-## potentially unnecessary match method
-func change_state(new_state):
-	match new_state:
-		ReticleState.MOUSE:
-			_enter_mouse_state(new_state)
-		ReticleState.CONTROLLER:
-			pass
-		ReticleState.DISABLED:
-			pass
-
-
-func _enter_mouse_state(new_state):
-	Input.set_custom_mouse_cursor(cursor_image)
-	current_state = new_state
 
 
 ## determine reticle position and state
@@ -35,21 +25,68 @@ func _enter_mouse_state(new_state):
 func _physics_process(delta: float) -> void:
 	match current_state:
 		ReticleState.MOUSE:
-			_process_mouse_state(delta)
+			_process_mouse_state()
 		ReticleState.CONTROLLER:
-			pass
+			_process_controller_state(delta)
 		ReticleState.DISABLED:
 			pass
 
 
-func _process_mouse_state(delta: float):
-	pass
+## potentially unnecessary match method
+func change_state(new_state):
+	match new_state:
+		ReticleState.MOUSE:
+			_enter_mouse_state()
+		ReticleState.CONTROLLER:
+			_enter_controller_state()
+		ReticleState.DISABLED:
+			pass
+
+
+func _enter_mouse_state():
+	current_state = ReticleState.MOUSE
+	Input.set_custom_mouse_cursor(cursor_image, 0, Vector2(16, 16))
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	sprite.visible = false
+
+
+## stay in mouse state until right stick is moved
+func _process_mouse_state():
+	if _get_reticle_input():
+		change_state(ReticleState.CONTROLLER)
+
+
+func _enter_controller_state():
+	current_state = ReticleState.CONTROLLER
+	.set_global_position(get_global_mouse_position())
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	sprite.visible = true
+	is_mouse_moving = false
+
+
+## stay in controller state until mouse is moved
+## move reticle according to right stick
+func _process_controller_state(delta: float):
+	if is_mouse_moving:
+		change_state(ReticleState.MOUSE)
+		return
+	velocity = _get_reticle_input() * MAX_SPEED
+	move_and_slide(velocity)
 
 
 ## sums up right stick input
 func _get_reticle_input() -> Vector2:
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = \
+			Input.get_action_strength("ui_reticle_right") \
+			- Input.get_action_strength("ui_reticle_left")
+	input_vector.y = \
+			Input.get_action_strength("ui_reticle_down") \
+			- Input.get_action_strength("ui_reticle_up")
 	return input_vector.normalized()
 
+
+# tracks if mouse is moving
+func _input(event):
+	if event is InputEventMouseMotion and event.relative:
+		is_mouse_moving = true
