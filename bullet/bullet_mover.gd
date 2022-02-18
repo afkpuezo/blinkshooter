2052f1badedb511be4758c7e1c06893bfb2948a7
afkpuezo <andrewfcurry@gmail.com>
2022-02-17 22:22:42 -0500

    bullet rotation handled

--- bullet/bullet_mover.gd
+++ bullet/bullet_mover.gd
@@ -7,7 +7,7 @@ onready var _stats: MovementStats = owner.get_node("MovementStats")
 
 
 func _ready() -> void:
-	_stats.velocity = Vector2(_stats.MAX_SPEED, 0)
+	_stats.velocity = Vector2(_stats.MAX_SPEED, 0).rotated(owner.rotation)
 
 
 func _physics_process(delta: float) -> void:
