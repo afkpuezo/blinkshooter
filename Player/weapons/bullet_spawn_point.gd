1f55301c8ea14e76deb58faf47fed6a0efe0a0f8
afkpuezo <andrewfcurry@gmail.com>
2022-03-17 13:22:21 -0400

    more skeletons of weapon system

---
+++ Player/weapons/bullet_spawn_point.gd
@@ -0,0 +1,4 @@
+extends Node2D
+class_name BulletSpawnPoint
+## Serves as a location for bullets to come out of on characters.
+
---
+++ Player/weapons/weapon.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name Weapon
+## This is an base class for simple individual player weapons. When triggered, creates the
+## appropriate bullet at the user's BulletSpawnPoint. Also keeps track of its own ammo
+## NOTE: may be used for enemy weapons at some point as well.
