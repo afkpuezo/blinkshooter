4580fd1999e3aed567fb9ae3bd3137670db522bf
afkpuezo <andrewfcurry@gmail.com>
2022-07-07 15:28:43 -0400

    better waiting in blink.gd

--- Player/action/blink.gd
+++ Player/action/blink.gd
@@ -4,6 +4,7 @@ class_name Blink
 
 
 export var MAX_RANGE := 500
+export var MIN_RANGE := 100
 export(PackedScene) var effect_scene
 
 
@@ -20,24 +21,17 @@ func can_do_action() -> bool:
 func do_action():
 	var target_position = TargetReticle.get_true_global_position()
 	var distance = user.position.distance_to(target_position)
+	if distance < MIN_RANGE:
+		return # TODO: explain why it failed?
 	if distance > MAX_RANGE:
 		# go as far as we can
 		var direction: Vector2 = \
 				user.position.direction_to(target_position).normalized()
 		target_position = user.position + (direction * MAX_RANGE)
+	# now actually teleport
 	if effect_scene:
-		GameSpawner.spawn_node(effect_scene.instance(), user.position)
+		GameSpawner.spawn_node(effect_scene.instance(), user.get_position)
 		GameSpawner.spawn_node(effect_scene.instance(), target_position)
 	user.do_teleport_animation()
-	_wait(0.25)
-
-	user.position = target_position # should this be global position?
-
-func _wait(time: float):
-	var t = Timer.new()
-	t.set_wait_time(time)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	yield(t, "timeout")
-	t.queue_free()
+	yield(get_tree().create_timer(0.25), "timeout")
+	user.set_position(target_position) # should this be global position?
