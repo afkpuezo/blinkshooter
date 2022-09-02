5a6b45e5c0da41ef90bf14566b522fa5a536217f
afkpuezo <andrewfcurry@gmail.com>
2022-09-01 23:17:49 -0400

    advanced enemy movement behavior

--- unit/enemy/enemy_mover/enemy_mover.gd
+++ unit/enemy/enemy_mover/enemy_mover.gd
@@ -5,6 +5,11 @@ class_name EnemyMover
 
 onready var player_detection: Area2D = $PlayerDetection
 
+export var stop_chasing_threshold := 200
+onready var chasing_squared = pow(stop_chasing_threshold, 2) # faster calculations apparently
+
+export var too_close_threshold := 100
+onready var too_close_squared = pow(too_close_threshold, 2)
 
 # -----
 # inherited from Mover
@@ -18,8 +23,15 @@ func physics_update(unit, movement_stats: MovementStats, delta: float):
 
 	if player:
 		unit.look_at(player.position)
-		var direction: Vector2 = (player.position - unit.position).normalized()
-		accelerate_towards(unit, movement_stats, delta, direction)
+		var distance_to_player = unit.position.distance_squared_to(player.position)
+		if distance_to_player > chasing_squared:
+			var direction: Vector2 = (player.position - unit.position).normalized()
+			accelerate_towards(unit, movement_stats, delta, direction)
+		elif distance_to_player < too_close_squared:
+			var direction: Vector2 = (player.position - unit.position).normalized() * -1
+			accelerate_towards(unit, movement_stats, delta, direction)
+		else: # if in the sweet spot
+			apply_friction(unit, movement_stats, delta)
 	else: # if no player found
 		apply_friction(unit, movement_stats, delta)
 
