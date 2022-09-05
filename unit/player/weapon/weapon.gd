0b101f61b251b9f0ecf2aa7e46c3c7c197b167ad
afkpuezo <andrewfcurry@gmail.com>
2022-09-05 14:42:31 -0400

    tweaked Weapon code

--- unit/player/weapon/weapon.gd
+++ unit/player/weapon/weapon.gd
@@ -5,33 +5,48 @@ class_name Weapon
 ## NOTE: may be used for enemy weapons at some point as well.
 
 export(PackedScene) var bullet_scene
-var spawn_location_source: Node2D
+var spawn_location: Node2D
 var user_movement_stats: MovementStats
 export var damage := 1
+
 
 # ----------
 # virtual methods from Action
 # ----------
 
+
 ## shoot da bullet
 ## TODO flesh out
 func do_action():
-	# spawn_location_source is set up here since the user var might not be configured at ready time
-	# should only happen once
-	if not spawn_location_source:
-		spawn_location_source = BulletSpawnPoint.get_bullet_spawn_point(user)
-		if not spawn_location_source:
-			spawn_location_source = user
-
-	# ditto with the setup here
-	if not user_movement_stats:
-		user_movement_stats = MovementStats.get_movement_stats(user) # assume it has one
+	_ready_spawn_location()
+	_ready_user_movement_stats()
 
 	var new_bullet: Bullet = bullet_scene.instance()
 	new_bullet.damage = damage
 
 	GameSpawner.spawn_bullet(
 		new_bullet,
-		spawn_location_source.get_global_position(),
+		spawn_location.get_global_position(),
 		user.get_global_rotation(),
 		user_movement_stats.velocity)
+
+
+# ----------
+# private / helper methods
+# ----------
+
+
+## spawn_location is set up here since the user var might not be configured at ready time
+## should only actually happen once
+func _ready_spawn_location():
+	if not spawn_location:
+		spawn_location = BulletSpawnPoint.get_bullet_spawn_point(user)
+		if not spawn_location:
+			spawn_location = user
+
+
+## user_movement_stats is set up here since the user var might not be configured at ready time
+## should only actually happen once
+func _ready_user_movement_stats():
+	if not user_movement_stats:
+		user_movement_stats = MovementStats.get_movement_stats(user) # assume it has one
