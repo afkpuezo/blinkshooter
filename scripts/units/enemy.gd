e2d23fe3fea58f2f919e08cac51111f01ab9e858
afkpuezo <andrewfcurry@gmail.com>
2022-09-29 21:17:15 -0400

    think() method in Enemy to handle choosing to shoot

--- scripts/units/enemy.gd
+++ scripts/units/enemy.gd
@@ -6,6 +6,13 @@ onready var player_detection: PlayerDetection = $PlayerDetection
 # seperate from radius of PlayerDetection, effectively the real threshold is the minimum of the two
 export var maximum_detection_range := 256
 onready var range_squared = pow(maximum_detection_range, 2)
+onready var weapon_bar: EnemyWeaponBar = $EnemyWeaponBar
+
+
+
+func _physics_process(delta: float) -> void:
+	._physics_process(delta) # movement in super
+	think()
 
 
 # -
@@ -13,6 +20,30 @@ onready var range_squared = pow(maximum_detection_range, 2)
 # new in Enemy
 # ----------
 # -
+
+
+# ----------
+# behavior related
+# ----------
+
+
+## holds behavior logic
+##		currently only attacks, movement is handled by the mover
+## should be called in _physics_process() (or _process()?)
+func think():
+	if should_attack():
+		attack()
+
+
+## returns true if this enemy should attack the player right now
+## currently only cares if the player is detected or not
+func should_attack() -> bool:
+	return player_detection.is_player_detected()
+
+
+## random equipped action
+func attack():
+	weapon_bar.trigger_random_action()
 
 
 # ----------
--- scripts/units/enemy_weapon_bar.gd
+++ scripts/units/enemy_weapon_bar.gd
@@ -5,7 +5,8 @@ class_name EnemyWeaponBar
 ## cooldown, picks one at random and triggers it
 
 
-onready var user: Enemy = get_parent()
+# DONT type hint as enemy, cyclical
+onready var user = get_parent()
 var actions := []
 var rng = RandomNumberGenerator.new()
 
@@ -18,14 +19,9 @@ func _ready() -> void:
 			c.configure_user(user) # this method is probably bad
 
 
-func _process(delta: float) -> void:
-	if user.is_player_detected():
-		var action = pick_action()
-		if action:
-			action.trigger()
-
-
-func pick_action() -> Action:
+## triggers a random (ready) action from the currently equipped actions
+## returns true/false depending if that actually happened
+func trigger_random_action() -> bool:
 	#print("DEBUG: pick_action() called")
 	var ready_actions := []
 	for a in actions:
@@ -33,7 +29,12 @@ func pick_action() -> Action:
 			ready_actions.append(a)
 			#print("DEBUG: pick_action() found ready action: %s" % a.name)
 
-	return ready_actions[rng.randi_range(0, ready_actions.size() - 1)] if ready_actions else null
+	if not ready_actions:
+		return false
+	else:
+		var action: Action = ready_actions[rng.randi_range(0, ready_actions.size() - 1)]
+		action.trigger()
+		return true
 
 
 
