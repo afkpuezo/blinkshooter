1fc8e79088ae55c9f9555c6852768c2c59fd0229
afkpuezo <andrewfcurry@gmail.com>
2022-11-02 20:46:53 -0400

    Revert "made HealthBar class"
    This reverts commit c805e8040058dd4eaaf5f105a17824fe625eb8dc.

--- scripts/ui/health_bar.gd
+++ scripts/ui/health_bar.gd
@@ -3,9 +3,5 @@ class_name HealthBar
 ## used for both enemies and player - will it just be resized or extended?
 
 
-## eventually could have a flashy animation
-func change_value(new_v, max_v):
-	value = new_v
-	max_value = max_v
 
 
