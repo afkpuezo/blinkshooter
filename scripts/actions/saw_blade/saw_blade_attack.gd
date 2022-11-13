74c23668c7e115a5ca77a0de58cb79d1be7bfa1d
afkpuezo <andrewfcurry@gmail.com>
2022-11-13 14:03:12 -0500

    AreaOverTimeAttack

--- scripts/actions/saw_blade/saw_blade_attack.gd
+++ scripts/actions/saw_blade/saw_blade_attack.gd
@@ -3,12 +3,7 @@ class_name SawBladeAttack
 ## this scene/node is the actual blade with the sprite and hurtbox, but not the
 ## action that uses/creates it
 
-var damage_cooldown_scene = load("res://scenes/buffs/damage_cooldown.tscn")
-
 var was_ready_called := false
-
-export var damage := 30 # should this be set in the action scene?
-export var damage_cooldown := 0.5
 
 export var sprite_rotation_speed_deg = 720 # per second?
 
@@ -35,43 +30,7 @@ func start() -> void:
 ## Checking collisions happens here because an enemy could stay in the hit box
 ## and continue to take damage (rather than using an on_entered signal)
 func _physics_process(delta: float) -> void:
-	handle_damage()
 	handle_sprite_rotation(delta)
-
-
-func handle_damage():
-	# NOTE: if i add more buff sources it may make sense to put some of this
-	# logic into a shared location
-	var victim_areas = _get_potential_victims()
-	for victim in victim_areas:
-		_handle_victim(victim)
-
-
-func _get_potential_victims() -> Array:
-	var areas: Array = hit_box.get_overlapping_areas()
-
-	var victims := []
-	for area in areas:
-		if area.has_method("get_unit"): # if it's a hurtbox
-			victims.append(area.get_unit())
-
-	return victims
-
-
-## If the victim can take buffs and does not have the DamageCooldown buff for
-## the sawblade, deal damage and apply the buff.
-func _handle_victim(victim):
-	if victim.has_method("get_buffs"):
-		var buffs: Array = victim.get_buffs()
-		for buff in buffs:
-			if buff.get("source") == self:
-				return
-
-		# if we got here, we can deal damage
-		var dc: DamageCooldown = damage_cooldown_scene.instance()
-		dc.setup(self, damage_cooldown)
-		victim.add_buff(dc)
-		victim.take_damage(damage, owner)
 
 
 func handle_sprite_rotation(delta):
