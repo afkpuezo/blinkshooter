bc1ee25e8390364a58d5ed5ad4494ecfd81754f5
afkpuezo <andrewfcurry@gmail.com>
2022-11-27 01:13:30 -0500

    progress on pickups

--- scenes/pickups/pickup.gd
+++ scenes/pickups/pickup.gd
@@ -3,57 +3,92 @@ class_name Pickup
 ## holds an item for the player to pick up by walking over it
 
 
-enum PICKUP_TYPE{small_gun, big_gun, blink, sawblade, shuriken, hp, small_ammo, big_ammo}
-export(PICKUP_TYPE) var type
-var item_scene
+enum TYPE{SMALL_GUN, BIG_GUN, BLINK, SAWBLADE, SHURIKEN, HEALTH, BASIC_AMMO, BIG_AMMO}
+enum META_TYPE{WEAPON, ACTION, RESOURCE}
+export(TYPE) var type
+var item_scene: PackedScene
+var meta_type: int
 
-# each type maps to a dict with the matching scene path, texture path, and radius
+# these are only actually used for resource pickups
+var resource_type: int # enum from CombatResource
+var resource_amount: int
+
+## each type maps to a dict with:
+##		matching scene path, texture path, radius, and meta type
+## resource pickups also have resource_type, resource_amount
+# NOTE: this is probably putting too much stuff in this one script...
 const TYPE_DETAILS = {
-	PICKUP_TYPE.small_gun: {
+	TYPE.SMALL_GUN: {
 		'scene': "res://scenes/actions/weapons/small_gun/small_gun.tscn",
 		'texture': "res://scenes/ui/player_ui/action_ui/assets/small_gun_item_oj.png",
 		'radius': 64,
+		'meta_type': META_TYPE.WEAPON,
 	},
-	PICKUP_TYPE.big_gun: {
+	TYPE.BIG_GUN: {
 		'scene': "res://scenes/actions/weapons/big_gun/big_gun.tscn",
 		'texture': "res://scenes/ui/player_ui/action_ui/assets/big_gun_item_oj.png",
 		'radius': 64,
+		'meta_type': META_TYPE.WEAPON,
 	},
-	PICKUP_TYPE.blink: {
+	TYPE.BLINK: {
 		'scene': "res://scenes/actions/blink/blink.tscn",
 		'texture': "res://scenes/ui/player_ui/action_ui/assets/blink_item.png",
 		'radius': 64,
+		'meta_type': META_TYPE.ACTION,
 	},
-	PICKUP_TYPE.sawblade: {
+	TYPE.SAWBLADE: {
 		'scene': "res://scenes/actions/saw_blade/saw_blade.tscn",
 		'texture': "res://scenes/ui/player_ui/action_ui/assets/sawblade_item.png",
 		'radius': 64,
+		'meta_type': META_TYPE.ACTION,
 	},
-	PICKUP_TYPE.shuriken: {
+	TYPE.SHURIKEN: {
 		'scene': "res://scenes/actions/shuriken/shuriken.tscn",
 		'texture': "res://scenes/ui/player_ui/action_ui/assets/shuriken_item.png",
 		'radius': 64,
+		'meta_type': META_TYPE.ACTION,
 	},
-	PICKUP_TYPE.hp: {
+	TYPE.HEALTH: {
 		'scene': "",
 		'texture': "",
 		'radius': 32,
+		'meta_type': META_TYPE.RESOURCE,
+		'resource_type': CombatResource.Type.HEALTH,
+		'resource_amount': 10
 	},
-	PICKUP_TYPE.small_ammo: {
+	TYPE.BASIC_AMMO: {
 		'scene': "",
 		'texture': "",
 		'radius': 32,
+		'meta_type': META_TYPE.RESOURCE,
+		'resource_type': CombatResource.Type.BASIC_AMMO,
+		'resource_amount': 20
 	},
-	PICKUP_TYPE.big_ammo: {
+	TYPE.BIG_AMMO: {
 		'scene': "",
 		'texture': "",
 		'radius': 32,
+		'meta_type': META_TYPE.RESOURCE,
+		'resource_type': CombatResource.Type.BIG_AMMO,
+		'resource_amount': 20
 	},
 }
 
-export var radius := 32
-
 var done := false
+
+
+# ----------
+# static funcs
+# ----------
+
+
+static func is_pickup(n):
+	return n != null and n.has_method("get_item")
+
+
+# ----------
+# instance funcs
+# ----------
 
 
 func _ready() -> void:
@@ -62,6 +97,7 @@ func _ready() -> void:
 	item_scene = load(details['scene']) # better to load here or later?
 	$CollisionShape2D.shape.radius = details['radius']
 	$Sprite.texture = load(details['texture'])
+	meta_type = details['meta_type']
 
 
 func get_item():
--- scenes/units/player/player_components/pickup_grabber.gd
+++ scenes/units/player/player_components/pickup_grabber.gd
@@ -1,14 +1,27 @@
 extends Area2D
 class_name PickupGrabber
 ## Hands an item to the player when they walk over one
+## Now takes care of figuring out what kind of pickup it is and sends it to the
+## correct Player component (through seperate signals)
 
 
-signal found_item(item)
+signal found_weapon(item)
+signal found_action(item)
+signal found_resource(item)
 
 
-func on_area_entered(area) -> void:
-	if area.has_method("get_item"):
-		emit_signal(
-			"found_item",
-			area.get_item()
-		)
+## TODO think about typing, it's all over the place here
+func on_area_entered(p: Pickup) -> void:
+	var signal_name: String
+	match p.meta_type:
+		Pickup.META_TYPE.ACTION:
+			signal_name = "found_action"
+		Pickup.META_TYPE.WEAPON:
+			signal_name = "found_weapon"
+		Pickup.META_TYPE.RESOURCE:
+			signal_name = "found_resource"
+
+	emit_signal(
+		signal_name,
+		p.get_item()
+	)
