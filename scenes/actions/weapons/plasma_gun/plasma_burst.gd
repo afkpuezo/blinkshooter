5102aab3342087ef7d0a677dc2f6e29b16d79240
afkpuezo <andrewfcurry@gmail.com>
2022-12-06 16:19:09 -0500

    started plasma burst

---
+++ scenes/actions/weapons/plasma_gun/plasma_burst.gd
@@ -0,0 +1,6 @@
+extends AreaOverTimeAttack
+tool # for warning icon
+class_name PlasmaBurst
+## when a plasma bullet explodes, it damages everything in an area. It also
+## applies a zap effect to each hit target
+
