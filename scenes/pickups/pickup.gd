0ecea52bc676915921382fcf2cd89cf6a14d1238
afkpuezo <andrewfcurry@gmail.com>
2022-12-05 23:44:53 -0500

    plasma gun and bullet

--- scenes/pickups/pickup.gd
+++ scenes/pickups/pickup.gd
@@ -6,7 +6,7 @@ class_name Pickup
 
 enum TYPE{
 	NONE,
-	SMALL_GUN, BIG_GUN,
+	SMALL_GUN, BIG_GUN, PLASMA_GUN,
 	BLINK, SAWBLADE, SHURIKEN,
 	HEALTH, BASIC_AMMO, BIG_AMMO, ENERGY
 }
@@ -38,6 +38,12 @@ const TYPE_DETAILS = {
 	},
 	TYPE.BIG_GUN: {
 		'scene': "res://scenes/actions/weapons/big_gun/big_gun.tscn",
+		'texture': "res://scenes/ui/player_ui/action_ui/assets/big_gun_item_oj.png",
+		'radius': 64,
+		'meta_type': META_TYPE.WEAPON,
+	},
+	TYPE.PLASMA_GUN: {
+		'scene': "res://scenes/actions/weapons/plasma_gun/plasma_gun.tscn",
 		'texture': "res://scenes/ui/player_ui/action_ui/assets/big_gun_item_oj.png",
 		'radius': 64,
 		'meta_type': META_TYPE.WEAPON,
