11aae4e53fcc61f503e68b0b82f596ef48fd2299
afkpuezo <andrewfcurry@gmail.com>
2022-12-09 23:16:06 -0500

    progress on overspeed in mover

--- scenes/movement/mover.gd
+++ scenes/movement/mover.gd
@@ -8,6 +8,17 @@ class_name Mover
 ## given (normalized) direction. Updates the unit's velocity, does not actually
 ## call move_and_slide.
 func accelerate_towards(movement_stats: MovementStats, delta: float, direction: Vector2):
+	var effective_max_speed = movement_stats.max_speed
+	if movement_stats.velocity.length() > effective_max_speed:
+		print("overspeed in %s. speed is %f and max is %d" % [get_parent().name, movement_stats.velocity.length(), effective_max_speed])
+		var resulting_speed = apply_friction(movement_stats)
+		effective_max_speed = max(resulting_speed, movement_stats.max_speed)
+
+	movement_stats.velocity = \
+			movement_stats.velocity.move_toward(direction * effective_max_speed, movement_stats.acceleration * delta)
+
+
+func accelerate_towards_OLD(movement_stats: MovementStats, delta: float, direction: Vector2):
 	movement_stats.velocity = \
 			movement_stats.velocity.move_toward(direction * movement_stats.max_speed, movement_stats.acceleration * delta)
 
@@ -15,11 +26,15 @@ func accelerate_towards(movement_stats: MovementStats, delta: float, direction: 
 ## Helper method, can be used in physics update, decelerates the unit using
 ## friction. Updates the unit's velocity, does not actually
 ## call move_and_slide.
-func apply_friction(movement_stats: MovementStats):
+## returns the resulting SPEED
+func apply_friction(movement_stats: MovementStats) -> float:
 	var multiplier: float = movement_stats.friction_percent * get_physics_process_delta_time()
 	movement_stats.velocity -= movement_stats.velocity * multiplier
-	if movement_stats.velocity.length() < 10:
+	var speed = movement_stats.velocity.length()
+	if speed < 10:
 		movement_stats.velocity = Vector2.ZERO
+		speed = 0
+	return speed
 
 
 ## helper method called to actually move the subject unit of a specific
