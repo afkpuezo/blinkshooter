a47eaeb95922495749e795ff8993f0424653f6be
afkpuezo <andrewfcurry@gmail.com>
2022-12-12 16:39:05 -0500

    pushing can scale with distance

--- scenes/actions/attacks/pusher.gd
+++ scenes/actions/attacks/pusher.gd
@@ -7,11 +7,29 @@ class_name Pusher
 
 
 export var strength := 100.0
+export var start_distance_falloff := 0.0
+export var end_distance_falloff := 100.0
+onready var falloff_distance_difference = end_distance_falloff - start_distance_falloff
 
 
 ## _arg lets this be called by a signal with 1 or 2 args
 func push(target: Node2D, _arg = null):
 	var movement_stats := MovementStats.get_movement_stats(target)
+
 	var force: Vector2 = global_position.direction_to(target.global_position) * strength
+	var distance_modifier = calculate_distance_modifier(target.global_position)
+	# doing it this way works for push and pull
+	force -= force * distance_modifier
+
 	movement_stats.apply_outside_force(force)
 
+
+func calculate_distance_modifier(target_position: Vector2) -> float:
+	var distance := global_position.distance_to(target_position)
+
+	if distance <= start_distance_falloff:
+		return 0.0
+	elif distance >= end_distance_falloff:
+		return 1.0
+	else:
+		return (distance - start_distance_falloff) / falloff_distance_difference
