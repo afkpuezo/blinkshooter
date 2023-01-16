e97a0f77b8030f38980c65cfe4e572511e385de2
afkpuezo <andrewfcurry@gmail.com>
2023-01-16 14:23:32 -0500

    pause menu works

--- scenes/singletons/pause_manager.gd
+++ scenes/singletons/pause_manager.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 ## handles pausing and displaying menu
 
 
+onready var margin_container := $MarginContainer
+
 var is_paused := false # should I just use the get_tree var?
 
 
@@ -11,9 +13,11 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("pause"):
 			is_paused = false
 			get_tree().paused = false
+			margin_container.visible = false
 		elif Input.is_action_just_pressed("quit"):
 			get_tree().quit()
 	else: # not paused
 		if Input.is_action_just_pressed("pause"):
 			is_paused = true
 			get_tree().paused = true
+			margin_container.visible = true
