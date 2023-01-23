a3558a0d378200070d7d2ca4b3b554e70830b658
afkpuezo <andrewfcurry@gmail.com>
2023-01-22 20:18:31 -0500

    fixed idling issues with isolated platforms

--- scenes/units/enemies/enemy_components/enemy_brain.gd
+++ scenes/units/enemies/enemy_components/enemy_brain.gd
@@ -190,7 +190,7 @@ func _think_chase(
 			# move closer to the player
 			# returns true if the nav agent indicates we have reached the last
 			# known position
-			if enemy_mover.move_to(this_unit, movement_stats, last_known_player_position) or enemy_mover.is_target_reachable():
+			if enemy_mover.move_to(this_unit, movement_stats, last_known_player_position) or (not enemy_mover.is_target_reachable()):
 				if not can_currently_see_player:
 					print("DEBUG: enemy %s going to IDLE" % this_unit.name)
 					current_mode = MODE.IDLE
