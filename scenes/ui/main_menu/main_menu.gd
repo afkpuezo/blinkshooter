451cc1003d3f15705f2e9b13b004601ee12b8a4b
afkpuezo <andrewfcurry@gmail.com>
2023-01-31 12:08:13 -0500

    progress on menus

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+ extends CanvasLayer
 class_name MainMenu
 ## it's the main menu
 
@@ -7,6 +7,10 @@ export var first_level_scene_path := "res://scenes/levels/level_1/level_1a.tscn"
 
 onready var timer: Timer = $Timer
 var is_working := false
+
+onready var main_menu_container: Container = $MainMenuContainer
+onready var level_select_container: Container = $LevelSelectContainer
+onready var no_levels_label: Label = $LevelSelectContainer/NoLevelsLabel
 
 
 func _ready() -> void:
@@ -27,3 +31,16 @@ func start_new_game():
 
 func quit_game():
 	get_tree().quit()
+
+
+func go_to_main_menu():
+	main_menu_container.visible = true
+	level_select_container.visible = false
+
+
+## adjusts the level select menu based on current save data
+func go_to_level_select():
+	main_menu_container.visible = false
+	level_select_container.visible = true
+
+
